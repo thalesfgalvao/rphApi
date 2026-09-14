f59284191b2db5fe2ea6b3dc4ece8b303096720b
Thales Marques <thalesf.galvao@gmail.com>
2026-09-14 13:54:24 -0300

    Feat: add createUser repository, service and controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,21 @@
 import { type Request, type Response } from "express";
-import { getUserById } from "../services/user.service.js";
+import {
+  getUserByIdService,
+  createUserService,
+} from "../services/user.service.js";
 
 export const getUserByIdController = async (req: Request, res: Response) => {
   const id = Number(req.params.id);
-  const user = await getUserById(id);
+  const user = await getUserByIdService(id);
   return res.status(200).json(user);
+};
+
+export const createUserController = async (req: Request, res: Response) => {
+  const { nick, email, password } = req.body;
+  const result = await createUserService(nick, email, password);
+
+  if (!result.success) {
+    return res.status(409).json(result);
+  }
+  return res.status(201).json(result);
 };
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,38 @@
+import { type RowDataPacket } from "mysql2";
 import pool from "../database/connection.js";
 
-export const findUserById = async (id: number) => {
-  const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
+export const getUserById = async (id: number) => {
+  const [rows] = await pool.query<RowDataPacket[]>(
+    "SELECT * FROM users WHERE id = ?",
+    [id],
+  );
+  return rows;
+};
+
+export const getUserByNick = async (nick: string) => {
+  const [rows] = await pool.query<RowDataPacket[]>(
+    "SELECT * FROM users WHERE nick = ?",
+    [nick],
+  );
+  return rows;
+};
+
+export const getUserByEmail = async (email: string) => {
+  const [rows] = await pool.query<RowDataPacket[]>(
+    "SELECT * FROM users WHERE email = ?",
+    [email],
+  );
+  return rows;
+};
+
+export const createUser = async (
+  nick: string,
+  email: string,
+  password: string,
+) => {
+  const [rows] = await pool.query(
+    "INSERT INTO users (nick, email, password) VALUES (?, ?, ?)",
+    [nick, email, password],
+  );
   return rows;
 };
