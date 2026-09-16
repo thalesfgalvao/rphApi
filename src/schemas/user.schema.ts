d39a693e9c5ffac425470b2d2f7ccaf2ac70fc3f
Thales Marques <thalesf.galvao@gmail.com>
2026-09-16 16:37:35 -0300

    Fix: adjust nick and password message in schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-  nick: z.string().min(2, "A senha deve possuir pelo menos 6 caracteres"),
+  nick: z.string().min(2, "O nick deve possuir pelo menos 2 caracteres"),
 
   email: z.string().email("Informe um email válido"),
 
   password: z
     .string()
-    .min(6)
+    .min(6, "A senha deve possuir pelo menos 6 caracteres")
     .regex(/[A-Z]/, "A senha deve possuir uma letra maiúscula")
     .regex(/[a-z]/, "A senha deve possuir uma letra minúscula")
     .regex(/[0-9]/, "A senha deve possuir um número")
