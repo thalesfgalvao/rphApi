86807214fb808cc53864f9f2a64275b1ae50f58a
Thales Marques <thalesf.galvao@gmail.com>
2026-09-16 16:40:16 -0300

    Feat: use http responses in user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,21 +1,22 @@
 import { createUserSchema } from "../schemas/user.schema.js";
 import { type Request, type Response } from "express";
+import { http } from "../constants/httpStatus.js";
 import {
   getUserByIdService,
   createUserService,
 } from "../services/user.service.js";
-
+const { success, badRequest, created } = http;
 export const getUserByIdController = async (req: Request, res: Response) => {
   const id = Number(req.params.id);
   const user = await getUserByIdService(id);
-  return res.status(200).json(user);
+  return res.status(success).json(user);
 };
 
 export const createUserController = async (req: Request, res: Response) => {
   const result = createUserSchema.safeParse(req.body);
 
   if (!result.success) {
-    return res.status(400).json({
+    return res.status(badRequest).json({
       message: "Dados inválidos",
       errors: result.error.issues.map((error) => ({
         field: error.path[0],
@@ -26,5 +27,5 @@ export const createUserController = async (req: Request, res: Response) => {
 
   const { nick, email, password } = result.data;
   const user = await createUserService(nick, email, password);
-  return res.status(201).json(user);
+  return res.status(created).json(user);
 };
