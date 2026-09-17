68bed2344e270102dfb7a78831b476e7d45c93a3
Thales Marques <thalesf.galvao@gmail.com>
2026-09-17 11:07:54 -0300

    Fix: include id in select query

--- src/repositories/login.repository.ts
+++ src/repositories/login.repository.ts
@@ -3,7 +3,7 @@ import pool from "../database/connection.js";
 
 export const getUserForLogin = async (nick: string) => {
   const [rows] = await pool.query<RowDataPacket[]>(
-    "SELECT nick, password, isAccountActive FROM users WHERE nick = (?)",
+    "SELECT id, nick, password, isAccountActive FROM users WHERE nick = (?)",
     [nick],
   );
   return rows;
