a30308b315eb015993d6b11f0cf94556c298a471
Thales Marques <thalesf.galvao@gmail.com>
2026-09-23 15:39:37 -0300

    Feat: Create authenticated route to get token

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import {
   createUserController,
+  getAuthenticatedUserController,
   getUserByIdController,
   getUserByNickController,
 } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/users/id/:id", getUserByIdController);
+router.get("/users/id/:id", authMiddleware, getUserByIdController);
 router.get("/users/member/:nick", getUserByNickController);
+router.get("/auth/me", authMiddleware, getAuthenticatedUserController);
 router.post("/users/", createUserController);
 
 export default router;
