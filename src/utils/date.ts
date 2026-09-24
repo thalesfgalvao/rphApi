f10deba59c0a6627e2bc6f011243bd70aabae409
Thales Marques <thalesf.galvao@gmail.com>
2026-09-24 14:00:36 -0300

    Feat: create formatIdentificationDate to format date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -25,3 +25,14 @@ export const calculateDaysBetweenDates = (startDate: Date, endDate: Date) => {
 
   return (endTimestamp - startTimestamp) / millisecondsPerDay;
 };
+
+export const formatIdentificationDate = (date: Date) => {
+  const formatter = new Intl.DateTimeFormat("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+
+  return formatter.format(date);
+};
