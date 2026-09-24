b75f4ce7eeee928330057eb5b9ac734fa6260dec
Thales Marques <thalesf.galvao@gmail.com>
2026-09-23 23:05:08 -0300

    Feat: create requeriments route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import requirementRoute from "./requirements.routes.js";
 
 const router = Router();
 
-router.use(userRoutes, authRoutes);
+router.use(userRoutes, authRoutes, requirementRoute);
 
 export default router;
